76c43559519eab8eac62f81fd5d064a7b2c7fc3c
rahul <rahul.chaube@ahytech.com>
2017-09-14 11:19:39 +0530

    firebase realtime database Added

--- app/src/main/java/com/demo/activity/Registration.java
+++ app/src/main/java/com/demo/activity/Registration.java
@@ -1,4 +1,4 @@
-package com.demo;
+package com.demo.activity;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
@@ -11,6 +11,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.demo.R;
+import com.demo.utitlity.Utility;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
--- app/src/main/java/com/demo/utitlity/Utility.java
+++ app/src/main/java/com/demo/utitlity/Utility.java
@@ -1,4 +1,4 @@
-package com.demo;
+package com.demo.utitlity;
 
 import android.app.ProgressDialog;
 import android.content.Context;
