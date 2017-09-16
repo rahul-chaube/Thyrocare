1b11bbbb0a8051c4ace2ba84fff0ee6aa87dbed6
rahul <rahul.chaube@ahytech.com>
2017-09-16 11:37:22 +0530

    gms Location is added

--- app/src/main/java/com/demo/activity/UserProfile.java
+++ app/src/main/java/com/demo/activity/UserProfile.java
@@ -1,9 +1,23 @@
 package com.demo.activity;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
+import android.app.Service;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +28,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.demo.model.FirebaseUserDetail;
@@ -21,6 +36,12 @@ import com.demo.model.UserDetail;
 import com.demo.utitlity.DataAttributes;
 import com.demo.R;
 import com.demo.utitlity.FirebaseConstant;
+import com.demo.utitlity.Utility;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -39,11 +60,15 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 import io.realm.Realm;
 
-public class UserProfile extends AppCompatActivity {
+public class UserProfile extends AppCompatActivity implements
+        View.OnClickListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
     ImageView imageButton;
     String uid, name, gender = "", yearOfBirth, careOf, villageTehsil, postOffice, district, state, postCode;
     EditText editTextName, editTextEmail, editTextMobile, editTextDob, editTextAddress, editTextPinCode, editTextMedicalHistory;
@@ -54,6 +79,27 @@ public class UserProfile extends AppCompatActivity {
     RadioGroup radioGroup;
     Realm realm;
     boolean allFilled = true;
+
+    //Location
+
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_LOCATION = 101;
+    TextView tvSetLocation;
+    Button btnGetLocation;
+
+    //Location
+    private boolean isLocationEnabled = false;
+    private Location userLocation;
+    private LocationManager locationManager;
+    String currentAddress;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,13 +114,16 @@ public class UserProfile extends AppCompatActivity {
         editTextDob = (EditText) findViewById(R.id.dob);
         editTextPinCode = (EditText) findViewById(R.id.pinCode);
         editTextMedicalHistory = (EditText) findViewById(R.id.medicalHistory);
+        locationManager = (LocationManager) this.getSystemService(Service.LOCATION_SERVICE);
 
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openScanner();
+
             }
         });
+
 
         buttonSave = (Button) findViewById(R.id.btnSave);
         {
@@ -121,6 +170,14 @@ public class UserProfile extends AppCompatActivity {
             }
         });
         initUI();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(UserProfile.this, "Getting Current location ", Toast.LENGTH_SHORT).show();
+                hello();
+
+            }
+        },1000);
     }
 
     void openScanner() {
@@ -344,5 +401,153 @@ public class UserProfile extends AppCompatActivity {
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.getDefault());
 
         editTextDob.setText(sdf.format(myCalendar.getTime()));
+    }
+
+
+    // Location
+
+  void  hello()
+  {
+
+      if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+              && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+          ActivityCompat.requestPermissions(this,
+                  new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                          android.Manifest.permission.ACCESS_FINE_LOCATION},
+                  REQUEST_LOCATION);
+      } else {
+          locationManager = (LocationManager) getSystemService(Service.LOCATION_SERVICE);
+          if (locationManager != null && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+              userLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+              if (userLocation == null) {
+                  LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, UserProfile.this);
+              }
+              if (userLocation != null) {
+                  getLocation(userLocation);
+              } else {
+                  buildAlertMessageNoGps();
+              }
+          } else buildAlertMessageNoGps();
+      }
+
+  }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mGoogleApiClient == null) {
+            initializeLocationVariables();
+        } else {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    private void initializeLocationVariables() {
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            mLocationRequest = LocationRequest.create()
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                    .setInterval(1000)
+                    .setFastestInterval(1000);
+
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApiIfAvailable(LocationServices.API)
+                    .build();
+            mGoogleApiClient.connect();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_LOCATION: {
+                //if permission denied permanentlygrantResults[i] == PackageManager.PERMISSION_DENIED
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                    if (Build.VERSION.SDK_INT >= 23 && !shouldShowRequestPermissionRationale(permissions[0])) {
+                        Toast.makeText(UserProfile.this, "Go to Settings and Grant the permission to use this feature.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    }
+
+    private void getLocation(Location userLocation) {
+        double latitude, longitude;
+        latitude = userLocation.getLatitude();
+        longitude = userLocation.getLongitude();
+        Log.e(TAG, String.valueOf(longitude));
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            for (int i = 0; i < addresses.size(); i++) {
+                Log.e(TAG,addresses.get(i).toString());
+            }
+            currentAddress = addresses.get(0).getAddressLine(0)+" "+addresses.get(0).getAddressLine(1)+addresses.get(0).getAddressLine(2)+addresses.get(0).getAddressLine(3);
+//            tvSetLocation.setText(currentAddress.trim());
+
+            editTextAddress.setText(currentAddress);
+            editTextPinCode.setText(addresses.get(0).getPostalCode());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.message_location_disabled_title))
+                .setMessage(getString(R.string.message_location_disabled_message))
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), 1);
+
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        Toast.makeText(UserProfile.this, R.string.allow_location_permission, Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        Toast.makeText(UserProfile.this, R.string.allow_location_permission, Toast.LENGTH_SHORT).show();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
